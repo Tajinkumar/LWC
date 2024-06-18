29e24add8f1ddc2d2237a4d71309ca294bdaab37
Tajinkumar <119869679+Tajinkumar@users.noreply.github.com>
2024-06-18 09:20:54 +0530

    Create LWC:Refs.js

---
+++ LWC:Refs.js
@@ -0,0 +1,12 @@
+import { LightningElement } from "lwc";
+
+export default class ParentComponent extends LightningElement {
+  parentUserName;
+
+  handleClickHandler(event) {
+   
+    this.parentUserName = this.template.querySelector(".childClass").value;
+  
+    console.log("this.parentUserName", this.parentUserName);
+  }
+}
